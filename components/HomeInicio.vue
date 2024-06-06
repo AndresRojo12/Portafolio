7230829572434020ce8a9773ede8cd02c2bb0818
AndresRojo12 <andresrojo20013@gmail.com>
2024-06-06 18:01:21 -0500

    se adapta para dispositivos moviles el home

--- components/HomeInicio.vue
+++ components/HomeInicio.vue
@@ -1,10 +1,29 @@
 <template>
   <v-app class="container">
     <nav class="navegation">
-      <div class="nav-content">
-        <img src="../util/logo.png" alt="Logo" class="logo"/>
-        <div class="nav-buttons">
-          <v-btn variant="text" :class="{ active: activeSection === 'presentation' }" @click="setActiveSection('presentation')">Presentación</v-btn>
+      <div
+        style="
+          color: white;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <img
+          style="border-radius: 50%; width: 300px;
+          margin-top: 2%;
+          margin-left: 1%;"
+          src="../util/logo.png"
+          alt=""
+        />
+        <div style="display: flex; gap: 10px; margin-left: auto">
+          <v-btn
+            variant="text"
+            :class="{ active: activeSection === 'presentation' }"
+            @click="setActiveSection('presentation')"
+          >
+            Presentación
+          </v-btn>
           <v-btn variant="text" @click="service">Servicios</v-btn>
           <v-btn variant="text" @click="proyect">Proyectos</v-btn>
         </div>
@@ -14,29 +33,53 @@
     <section class="section1">
       <h1>HOLA MI NOMBRE ES ANDRES</h1>
       <h2>Desarrollo aplicaciones web</h2>
-      <p>Soy desarrollador de software, desde que me adentré en el mundo de la programación he estado obsesionado con encontrar soluciones creativas a problemas complejos y crear productos tecnológicos que impacten de manera positiva en la vida de las personas.</p>
+      <P>
+        Soy desarrollador de software, desde que me adentré en el mundo de la
+        programación <br />
+        he estado obsesionado con encontrar soluciones creativas a problemas
+        complejos y crear productos tecnológicos <br />
+        que impacten de manera positiva en la vida de las personas.
+      </P>
     </section>
 
     <section class="section2">
       <h3>Habilidades destacadas</h3>
       <div class="images">
-        <div class="skill"><icon icon="logos:javascript" alt="JavaScript" /></div>
-        <div class="skill"><icon icon="logos:nodejs" alt="Node.js" /></div>
-        <div class="skill"><icon icon="simple-icons:express" alt="Express" /></div>
-        <div class="skill"><icon icon="logos:vue" alt="Vue.js" /></div>
-        <div class="skill"><icon icon="logos:vuetifyjs" alt="Vuetify.js" /></div>
+        <div class="js"><icon icon="logos:javascript" alt="JavaScript" /></div>
+        <div class="node"><icon icon="logos:nodejs" alt="Node.js" /></div>
+        <div class="ex"><icon icon="simple-icons:express" alt="Express" /></div>
+        <div class="vue"><icon icon="logos:vue" alt="Vue.js" /></div>
+      </div>
+      <div class="fr">
+        <div class="nuxt"><icon icon="logos:nuxt" alt="Vue.js" /></div>
+        <div class="vuetify"><icon icon="logos:vuetifyjs" alt="Vue.js" /></div>
       </div>
     </section>
-    
-    <v-footer class="footer">
-      <div class="footer-top">
+    <hr class="separator" />
+
+    <v-footer class="d-flex flex-column" style="background-color: black">
+      <div
+        style="background-color: rgb(151, 157, 142)"
+        class="bg-black d-flex w-100 align-center px-4"
+      >
         <strong>Contacto</strong>
         <v-spacer></v-spacer>
-        <a v-for="icon in icons" :key="icon.name" :href="icon.url" target="_blank">
-          <v-btn :icon="icon.name" class="footer-icon"></v-btn>
+        <a
+          v-for="icon in icons"
+          :key="icon.name"
+          :href="icon.url"
+          target="_blank"
+        >
+          <v-btn
+            :icon="icon.name"
+            class="bg-black mx-4"
+            size="small"
+            variant="plain"
+          ></v-btn>
         </a>
       </div>
-      <div class="footer-bottom">
+
+      <div class="px-4 py-2 bg-black text-center w-100">
         {{ new Date().getFullYear() }} — <strong>Andres Rojo</strong>
       </div>
     </v-footer>
@@ -44,15 +87,15 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useRuntimeConfig } from '#app';
+import { ref } from "vue";
+import { useRuntimeConfig } from "#app";
 const CONFIG = useRuntimeConfig().public;
 const router = useRouter();
 
 const icons = ref([
   { name: "mdi-github", url: CONFIG.GIT_HUB },
   { name: "mdi-gmail", url: `mailto:${CONFIG.CORREO}` },
-  { name: "mdi-linkedin", url: CONFIG.LINKEDIN }
+  { name: "mdi-linkedin", url: CONFIG.LINKEDIN },
 ]);
 
 const activeSection = ref("presentation");
@@ -70,108 +113,81 @@ const proyect = () => {
 };
 </script>
 
-
 <style scoped>
-body, .container {
+body,
+.container,
+.d-flex flex-column {
   background-color: #000;
+}
+.section1 {
+  background-color: #000;
+  margin-top: 5%;
+  margin-left: 5%;
+}
+.section1 h1 {
+  color: aqua;
+  margin-top: 2%;
+  margin-left: 2%;
+  font-size: 200%;
+}
+.section1 p,
+h1,
+h2 {
   color: azure;
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
 }
-
-.nav-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1% 2%;
+.section1 h2 {
+  margin-top: 1%;
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 500%;
 }
-
-.logo {
-  width: 300px;
-  border-radius: 50%;
+.section1 p {
+  margin-top: 2%;
+  font-size: 200%;
+  font-family: "Times New Roman", Times, serif;
 }
-
-.nav-buttons {
-  display: flex;
-  gap: 10px;
+.section2 {
+  background: color #000;
+  margin-left: 5%;
+  margin-top: 5%;
 }
-
-.section1, .section2 {
-  padding: 5% 10%;
-  text-align: center;
-}
-
-.section1 h1, .section1 h2, .section1 p {
-  margin: 20px 0;
+.section2 h3 {
+  color: azure;
+  font-size: 200%;
 }
 
 .images {
   display: flex;
-  flex-wrap: wrap;
   justify-content: center;
-  gap: 60px;
-}
-
-.skill {
-  font-size: 300px;
-}
-
-.footer {
-  background-color: black;
-  color: white;
-  
-  text-align: center;
-}
-
-.footer-top {
-  justify-content: space-between;
   align-items: center;
-  margin-left: 100px;
-  
-}
-
-.footer-icon {
   margin: 0 10px;
-  margin-top: 5%;
- 
+  font-size: 200px;
+}
+.images div {
+  margin-top: 2%;
+}
+.fr {
+  display: flex;
+  margin: 0 10px;
+  font-size: 100px;
+  margin-left: 5%;
 }
 
-.footer-bottom {
-  margin-left: 50px;
-  margin-top: 30px;
+.node,
+.ex,
+.vue {
+  padding-left: 8%;
+  color: azure;
 }
 
 
-@media (max-width: 768px) {
-  .nav-content {
-    flex-direction: column;
-  }
-
-  .logo {
-    width: 80px;
-  }
-
-  .section1 h1, .section1 h2, .section1 p {
-    font-size: 18px;
-  }
-
-  .skill {
-    font-size: 40px;
-  }
+.separator {
+  border: none;
+  height: 1px;
+  background-color: rgb(59, 62, 64);
+  margin: 20px 0;
 }
 
-@media (max-width: 480px) {
-  .nav-buttons {
-    flex-direction: column;
-  }
-
-  .section1 h1, .section1 h2, .section1 p {
-    font-size: 16px;
-  }
-
-  .skill {
-    font-size: 30px;
-  }
+.v-btn.active {
+  border-bottom: 2px solid aqua;
 }
 </style>
