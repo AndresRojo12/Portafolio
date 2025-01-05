bbc6d30046b00b034e659c97302c720cfe677e2b
AndresRojo12 <andresrojo20013@gmail.com>
2025-01-05 15:32:01 -0500

    se ajusta el homeInicio

--- components/HomeInicio.vue
+++ components/HomeInicio.vue
@@ -18,7 +18,7 @@
 
     <section class="section1">
       <h1>HOLA MI NOMBRE ES ANDRES</h1>
-      <h2>Desarrollador de software</h2>
+      <h2 style="font-size:50px ;">Desarrollador de software</h2>
       <p>
         Soy desarrollador de software, desde que me adentré en el mundo de la
         programación he estado obsesionado con encontrar soluciones creativas a
@@ -33,7 +33,7 @@
         <div class="skill">
           <h2 style="font-size: 50px">Desarrollo Web</h2>
           <img
-            style="width: 500px; height: 300px"
+            style="width: 480px; height: 300px"
             src="../util/33.png"
             alt="Node.js"
           />
@@ -41,7 +41,7 @@
         <div class="skill">
           <h2 style="font-size: 50px">Análisis de datos</h2>
           <img
-            style="width: 500px; height: 300px"
+            style="width: 480px; height: 300px"
             src="../util/datos-y-graficas-relevantes.jpg"
             alt="Express"
           />
@@ -50,105 +50,82 @@
     </section>
     <section class="section2">
       <h3>Lenguajes y Frameworks De Desarrollo</h3>
-      <table>
-        <tr>
-          <td style="width: 50%">
-            <br />
+      <div style="margin-top: 2%;" class="images">
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="../util/javascript.png"
+            alt="JavaScript"
+          />
+        </div>
 
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="../util/javascript.png"
-              alt="JavaScript"
-            />
-          </td>
-          <td style="width: 50%">
-            <br />
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/26a17d10e39adb59c0b8efef533475ef57c726490d9dcf022558d3b4d3fd4d03/68747470733a2f2f692e626c6f67732e65732f3864323432302f3635305f313030305f6a6176612f3435305f313030302e77656270"
-              alt="Java"
-            />
-          </td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <td style="width: 50%">
-            <br />
+        <br />
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/26a17d10e39adb59c0b8efef533475ef57c726490d9dcf022558d3b4d3fd4d03/68747470733a2f2f692e626c6f67732e65732f3864323432302f3635305f313030305f6a6176612f3435305f313030302e77656270"
+            alt="Java"
+          />
+        </div>
 
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/7a51fd739b8842b77b72a23ecfc9733f369d41ae604298657738e3c45d03d476/68747470733a2f2f6a6168617a69656c706f6e63652e636f6d2f77702d636f6e74656e742f75706c6f6164732f707974686f6e2e706e67"
-              alt="Python"
-            />
-          </td>
-          <td style="width: 50%">
-            <br />
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/f79e907b575f52746df0681cf1303c95afca425fd7ae9d226e07900bd0838c90/68747470733a2f2f7777772e6d75796c696e75782e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032322f30342f6e6f64656a732e706e67"
-              alt="node.js"
-            />
-          </td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <td style="width: 50%">
-            <br />
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/7a51fd739b8842b77b72a23ecfc9733f369d41ae604298657738e3c45d03d476/68747470733a2f2f6a6168617a69656c706f6e63652e636f6d2f77702d636f6e74656e742f75706c6f6164732f707974686f6e2e706e67"
+            alt="Python"
+          />
+        </div>
 
-            <div class="skill">
-              <img
-                style="width: 500px; height: 300px; padding: 2%"
-                src="https://camo.githubusercontent.com/7b098769578864ba3ba2d3a777a5e0468d42d06379b55ccc8a07fb0ba465fda8/68747470733a2f2f696d672e69636f6e73382e636f6d2f636f6c6f722f3531322f657870726573732d6a732e706e67"
-                alt=""
-              />
-            </div>
-          </td>
-          <td style="width: 50%">
-            <br />
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/3aec4c13cdb91e36a6acc0254e7bb1d4458bc73ff7f42d8e61666c0050543296/68747470733a2f2f6b696e7374612e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032322f30362f6e6573742d6a732d6c6f676f2e706e67"
-              alt="nest"
-            />
-          </td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <td style="width: 50%">
-            <br />
+        <br />
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/f79e907b575f52746df0681cf1303c95afca425fd7ae9d226e07900bd0838c90/68747470733a2f2f7777772e6d75796c696e75782e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032322f30342f6e6f64656a732e706e67"
+            alt="node.js"
+          />
+        </div>
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/7b098769578864ba3ba2d3a777a5e0468d42d06379b55ccc8a07fb0ba465fda8/68747470733a2f2f696d672e69636f6e73382e636f6d2f636f6c6f722f3531322f657870726573732d6a732e706e67"
+            alt=""
+          />
+        </div>
 
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/f789d687e85d0a7e6c0589c86574a5384e2aed8b7982b2e3f0ca7dc771896454/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f76322f726573697a653a6669743a313030302f312a714a7070544d64755858686a675532745a74395366512e706e67"
-              alt=""
-            />
-          </td>
-          <td style="width: 50%">
-            <br />
-            <img
-              style="width: 500px; height: 300px; padding: 2%"
-              src="https://camo.githubusercontent.com/ef447d27faeac53acdead46603956566096a1be47cae1f60f1fe45c24393ef6b/68747470733a2f2f6170692e6f74616b6f79692e736f6674776172652f75706c6f6164732f636f6e74656e742f323032332f31312f32302f313238302f7675656a732d616e642d73656f2d2d796f75722d73746570732d746f2d74616b652d746f2d6265636f6d652d6d6f72652d73656f2d667269656e646c792d333137783233372e77656270"
-              alt=""
-            />
-          </td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <td style="width: 50%">
-            <br />
+        <br />
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/3aec4c13cdb91e36a6acc0254e7bb1d4458bc73ff7f42d8e61666c0050543296/68747470733a2f2f6b696e7374612e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032322f30362f6e6573742d6a732d6c6f676f2e706e67"
+            alt="nest"
+          />
+        </div>
 
-            <img
-              style="width: 500px; height: 300px"
-              src="https://camo.githubusercontent.com/aafdcccc13673a95eee71819045e11c5a61a1d560129fc3c9affd46cdc5c3b9b/68747470733a2f2f63646e2e767565746966796a732e636f6d2f696d616765732f6c6f676f732f6c6f676f2e737667"
-              alt=""
-            />
-          </td>
-        </tr>
-      </table>
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/f789d687e85d0a7e6c0589c86574a5384e2aed8b7982b2e3f0ca7dc771896454/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f76322f726573697a653a6669743a313030302f312a714a7070544d64755858686a675532745a74395366512e706e67"
+            alt=""
+          />
+        </div>
+
+        <br />
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px;"
+            src="https://camo.githubusercontent.com/ef447d27faeac53acdead46603956566096a1be47cae1f60f1fe45c24393ef6b/68747470733a2f2f6170692e6f74616b6f79692e736f6674776172652f75706c6f6164732f636f6e74656e742f323032332f31312f32302f313238302f7675656a732d616e642d73656f2d2d796f75722d73746570732d746f2d74616b652d746f2d6265636f6d652d6d6f72652d73656f2d667269656e646c792d333137783233372e77656270"
+            alt=""
+          />
+        </div>
+
+        <div class="skill">
+          <img
+            style="width: 480px; height: 300px"
+            src="https://camo.githubusercontent.com/aafdcccc13673a95eee71819045e11c5a61a1d560129fc3c9affd46cdc5c3b9b/68747470733a2f2f63646e2e767565746966796a732e636f6d2f696d616765732f6c6f676f732f6c6f676f2e737667"
+            alt=""
+          />
+        </div>
+      </div>
     </section>
 
     <hr class="separator" />
@@ -183,7 +160,6 @@ const icons = ref([
   { name: "mdi-gmail", url: `mailto:${CONFIG.CORREO}` },
   { name: "mdi-linkedin", url: CONFIG.LINKEDIN },
   { name: "mdi-whatsapp", url: CONFIG.WPP },
-
 ]);
 
 const activeSection = ref("presentation");
@@ -316,7 +292,10 @@ body,
   .nav-buttons {
     flex-direction: column;
   }
-
+  .section1 h2{
+    font-size: 20px;
+  }
+  
   .section1 h1,
   .section1 h2,
   .section1 p {
