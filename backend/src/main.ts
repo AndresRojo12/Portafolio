bd80eaf7eee53fd51a7fbf14e21eec734706ba3f
AndresRojo12 <andresrojo20013@gmail.com>
2025-11-26 13:34:54 -0500

    se agrega un log

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('MAIL_USER:', process.env.MAIL_USER);
 
   app.enableCors({
     origin: '*',
