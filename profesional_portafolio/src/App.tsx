5ec92802a5350a27f856508d63066fc989ed38da
AndresRojo12 <andresrojo20013@gmail.com>
2025-11-28 12:52:27 -0500

    se actualiza estilo de colores

--- profesional_portafolio/src/App.tsx
+++ profesional_portafolio/src/App.tsx
@@ -120,10 +120,10 @@ export default function Portfolio() {
     <>
       <div className="min-h-screen bg-gray-50 text-gray-900 antialiased">
         {/* HEADER */}
-        <header className="w-full px-6 md:px-10 py-4 fixed top-0 left-0 bg-white/90 backdrop-blur-sm shadow-sm z-[999]">
+        <header className="dark:bg-gray-800 w-full px-6 md:px-10 py-4 fixed top-0 left-0 bg-white/90 backdrop-blur-sm shadow-sm z-[999]">
 
           <div className="max-w-6xl mx-auto flex items-center justify-between">
-            <a href="#" className="flex items-center gap-3">
+            <a href="/" className="flex items-center gap-3">
               <div className="w-12 h-12 rounded-full bg-gradient-to-br from-green-400 to-blue-500 flex items-center justify-center text-white font-bold overflow-hidden">
                 <img
                   src="/logo.png"
@@ -132,7 +132,7 @@ export default function Portfolio() {
                 />
               </div>
               <div className="hidden md:block">
-                <p className="text-sm font-semibold">ANDRES ROJO</p>
+                <p className="text-sm text-teal-500">ANDRES ROJO</p>
                 <p className="text-xs text-gray-500">
                   Ingeniero de Software & Datos
                 </p>
@@ -140,17 +140,17 @@ export default function Portfolio() {
             </a>
 
             {/* NAV escritorio */}
-            <nav className="hidden md:flex gap-6 text-sm font-medium">
-              <a href="#presentacion" className="hover:text-green-600">
+            <nav className="text-gray-500 hidden md:flex gap-6 text-sm font-medium">
+              <a href="#presentacion" className="hover:text-teal-500">
                 Presentación
               </a>
-              <a href="#formacion" className="hover:text-green-600">
+              <a href="#formacion" className="hover:text-teal-500">
                 Formación
               </a>
-              <a href="#proyectos" className="hover:text-green-600">
+              <a href="#proyectos" className="hover:text-teal-500">
                 Proyectos
               </a>
-              <a href="#contacto" className="hover:text-green-600">
+              <a href="#contacto" className="hover:text-teal-500">
                 Contacto
               </a>
             </nav>
@@ -249,7 +249,7 @@ export default function Portfolio() {
             className="max-w-6xl mx-auto px-6 md:px-10 py-16 grid md:grid-cols-2 gap-10 items-center"
           >
             <div>
-              <p className="text-green-600 font-semibold">Hola — soy</p>
+              <p className="text-teal-500 font-semibold">Hola — soy</p>
               <h1 className="text-4xl md:text-5xl font-extrabold leading-tight mt-2">
                 ANDRES ROJO
               </h1>
@@ -414,7 +414,7 @@ export default function Portfolio() {
                   className="bg-white rounded-2xl shadow p-6 hover:shadow-lg transition"
                 >
                   <h3 className="font-semibold text-lg">{p.title}</h3>
-                  <p className="text-sm text-green-600 mt-2">{p.tech}</p>
+                  <p className="text-sm text-teal-600 mt-2">{p.tech}</p>
                   <p className="mt-3 text-gray-600">{p.desc}</p>
                   <div className="mt-4 flex items-center justify-between">
                     <a
@@ -424,7 +424,7 @@ export default function Portfolio() {
                     >
                       Ver detalles →
                     </a>
-                    <div className="text-xs text-gray-400">2024 • Demo</div>
+                    <div className="text-xs text-gray-400">2024 - 2025 • Demo</div>
                   </div>
                 </article>
               ))}
@@ -448,7 +448,7 @@ export default function Portfolio() {
                     <p className="font-semibold">Email</p>
                     <a
                       href={import.meta.env.VITE_EMAIL}
-                      className="text-green-600"
+                      className="text-gray-500"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -466,7 +466,7 @@ export default function Portfolio() {
                       href={import.meta.env.VITE_WHATSAPP}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-green-600"
+                      className="text-green-500"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -530,14 +530,14 @@ export default function Portfolio() {
                   <div className="flex items-center gap-3">
                     <button
                       type="submit"
-                      className="bg-green-500 text-white px-4 py-2 rounded-md font-semibold"
+                      className="bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white px-4 py-2 rounded-md font-semibold"
                     >
                       Enviar
                     </button>
                     <button
                       type="button"
                       onClick={clearForm}
-                      className="border px-3 py-2 rounded-md"
+                      className="border px-3 py-2 rounded-md bg-white-500 shadow-lg shadow-white-500/50 text-gray-500 font-semibold"
                     >
                       Limpiar
                     </button>
@@ -548,7 +548,7 @@ export default function Portfolio() {
           </section>
 
           {/* FOOTER */}
-          <footer className="mt-8 py-8 border-t">
+          <footer className="mt-8 py-8 border-t ">
             <div className="max-w-6xl mx-auto px-6 md:px-10 flex flex-col md:flex-row items-center justify-between text-sm text-gray-500 gap-6">
               <div>© {new Date().getFullYear()} Andrés Rojo.</div>
 
